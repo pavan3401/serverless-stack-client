bceec3076ee9f4efb266640794461885e5b37e24
koneru <koneru@upenn.edu>
2019-11-05 13:06:41 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-9876123"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://33zljoy9xd.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_pdhK5gBiy",
+    APP_CLIENT_ID: "60j4paoq63h00hg6n8gqbos1ic",
+    IDENTITY_POOL_ID: "us-east-1:d3c5718c-3fb1-4c6f-a7a1-517931f9bfbe"
+  }
+};
